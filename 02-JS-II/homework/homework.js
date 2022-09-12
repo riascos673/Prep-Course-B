e8324cf341424829b56abaa14e0af47d1934143f
Diego Riascos <riascos673@gmail.com>
2022-09-12 18:53:08 -0500

    primer commit

--- 02-JS-II/homework/homework.js
+++ 02-JS-II/homework/homework.js
@@ -1,4 +1,4 @@
-// En los próximos 22 problemas, deberás completar la función.
+// En los próximos 22 problemas, deberás completar la función
 // Todo tu código irá dentro de las llaves de la función.
 // Asegúrate que usas "return" cuando la consola te lo pida.
 // Pista: "console.log()" NO fucionará.
